b452b63d7e3a2bbe3e6823b7bd6ab34a480282ff
Sahildhillon0 <sahildhillon609@gmail.com>
2024-11-26 22:24:03 +0530

    Initializing bank management project

---
+++ hash_java/learning_hell/Initials/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your name :- ");
+        String name = sc.nextLine();
+        System.out.print("Enter your Age :- ");
+        int x= sc.nextInt();
+        System.out.println(x+"! Welcome "+name + "!");
+    }
+}
